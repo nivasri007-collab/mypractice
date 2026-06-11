0290a423ddb04b23369f33ea6b85dfe1bf110ab4
nivasri007-collab <nivasri007@gmail.com>
2026-06-11 13:08:30 +0530

    chenged

--- mypractice/src/App.jsx
+++ mypractice/src/App.jsx
@@ -8,16 +8,11 @@ function App() {
   
 
   return (
-    <>
-<Routes>
-  <Route path='/home' element={<HomePage/>} />
-  <Route path='about' element={<AboutPage/>}/>
-  <Route path='/contact' element={<ContactPage/>}/>
-</Routes>
-{/*     <HomePage />
-    <AboutPage />
-    <ContactPage />
- */}    </>
+  <>
+  <HomePage />
+  <AboutPage />
+  <ContactPage />
+  </>
   )
 }
 
