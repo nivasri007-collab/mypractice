0d3450083777850a30215f530bc69012f60ba56d
nivasri007-collab <nivasri007@gmail.com>
2026-06-11 09:18:17 +0530

    added mypractice

---
+++ mypractice/src/App.jsx
@@ -0,0 +1,19 @@
+
+/* import './App.css'
+ */
+import HomePage from "./pages/Home"
+import AboutPage from "./pages/About"
+import ContactPage from "./pages/Contact"
+function App() {
+  
+
+  return (
+    <>
+    <HomePage />
+    <AboutPage />
+    <ContactPage />
+    </>
+  )
+}
+
+export default App
---
+++ mypractice/src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function ContactPage(){
+    return(
+        <>
+        <h1>This is Contact Page</h1>
+        </>
+    )
+}
+export default ContactPage
